be4c5b4d7b95032d176b47fabc834700267281fe
workspace <gusto@cometnet-10-21-20-156.utdallas.edu>
2017-11-18 17:00:41 -0600

    updates on the view

--- client/src/containers/auth/AuthenticateView.js
+++ client/src/containers/auth/AuthenticateView.js
@@ -12,14 +12,17 @@ import {
   Image,
   StyleSheet,
 } from 'react-native';
-import { Actions } from 'react-native-router-flux';
 
+import { Actions } from 'react-native-router-flux';
 // Consts and Libs
 import { AppStyles, AppSizes, AppColors } from '@theme/';
 
 // Components
-import { Spacer, Text, Button } from '@ui/';
+import { Spacer, Text,Button} from '@ui/';
 
+/* Styles ==================================================================== */
+/* Styles ==
+/* Styles ==
 /* Styles ==================================================================== */
 const styles = StyleSheet.create({
   background: {
@@ -28,8 +31,12 @@ const styles = StyleSheet.create({
     width: AppSizes.screen.width,
   },
   logo: {
-    width: AppSizes.screen.width * 0.85,
+    width: AppSizes.screen.width * 0.6,
     resizeMode: 'contain',
+  },
+  blue_arrow: {
+    width:  120,
+    height: 120,
   },
   whiteText: {
     color: '#FFF',
@@ -41,28 +48,47 @@ class Authenticate extends Component {
   static componentName = 'Authenticate';
 
   render = () => (
+
     <View style={[AppStyles.containerCentered, AppStyles.container, styles.background]}>
+
       <Image
         source={require('../../images/logo.png')}
         style={[styles.logo]}
       />
 
+      <Text style={{fontSize: 20,textAlign: 'center',color:'#000', margin: 20,fontWeight: 'bold'}}>
+        Secure identity management built on blockchain
+     </Text>
 
-      <View style={[AppStyles.row, AppStyles.paddingHorizontal]}>
+        <Text style={{fontSize: 18,textAlign: 'center',color:'#000', margin: 20, fontFamily:'Cochin'}}>
+         Infinity allows you to share your identity with trusted Infinity parterns. We believe personal identity is intimate,
+         which is why Infinity puts you, the user, in control of who you share your identity with.
+      </Text>
+
+
+<Spacer size={50} />
+
+      <View style={[AppStyles.row, AppStyles.paddingHorizontal,AppStyles.container,AppStyles.containerCentered]}>
+      <Image
+           source={require('../../images/shield.png')}
+           style={[styles.blue_arrow]}
+      />
+
         <View style={[AppStyles.flex1]} />
-        <View style={[AppStyles.flex2]}>
+          <Spacer size={35} />
+        <View style={[AppStyles.flex6]}>
           <Button
-            small
-            title={'Create New Infinity ID'}
-            onPress={Actions.app}
-            raised={false}
-            backgroundColor={'#D6D6D6'}
+          color={'#000'}
+          title='Create New Infinity ID'
+          onPress={Actions.app}
+          raised={false}
+          backgroundColor={'#d6d6d6'}
           />
+
         </View>
         <View style={[AppStyles.flex1]} />
       </View>
 
-      <Spacer size={40} />
     </View>
   )
 }
